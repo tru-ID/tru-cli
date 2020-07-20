0deb10dd0c4ebd0750e58c1caf66984d0d3a3f70
Phil Leggetter <phil@leggetter.co.uk>
2020-07-20 22:10:00 +0100

    Create directory and 4auth.json config for new project

--- src/commands/projects/create.ts
+++ src/commands/projects/create.ts
@@ -3,6 +3,9 @@ import CommandWithConfig from '../commandWithConfig'
 import * as inquirer from 'inquirer'
 import {Projects} from '../../api/projects'
 import {APIConfiguration} from '../../api/APIConfiguration'
+import {stringToSnakeCase} from '../../utilities'
+import { string } from '@oclif/command/lib/flags'
+import * as fs from 'fs-extra'
 
 export default class Create extends CommandWithConfig {
   static description = 'Creates a new Project'
@@ -52,6 +55,26 @@ Creating Project "My first project"
         name: args.name
     })
 
-    console.log(projectCreationResult.data)
+    const directoryName = stringToSnakeCase(args.name)
+    const directoryToCreate = `${process.cwd()}/${directoryName}`
+    if(fs.existsSync(directoryToCreate)) {
+        this.error(`Cannot create project directory "${directoryToCreate}" because a directory with that name already exists.\n` +
+                   `Please choose another name for your project.`, {exit: 1})
+    }
+    else {
+      const configFileFullPathToCreate = `${directoryToCreate}/4auth.json`
+      try {
+        // Save the project configuration to match the Project resource excluding the _links property
+        const projectConfig = {
+          ...projectCreationResult.data
+        }
+        delete projectConfig._links
+        await fs.outputJson(configFileFullPathToCreate, projectConfig, {spaces: '\t'})
+      }
+      catch(error) {
+        this.error(`An unexpected error occurred: ${error}`, {exit: 1})
+      }
+    }
   }
+
 }
--- test/commands/projects.create.test.ts
+++ test/commands/projects.create.test.ts
@@ -20,72 +20,146 @@ let expectedUserConfig = {
 }
 
 let existsSyncStub:any = null
+let projectConfigFileCreationStub:any
+let newProjectName: string = 'My First Project'
+let expectedProjectDirectoryName = 'my_first_project'
+const expectedProjectFullPath = `${process.cwd()}/${expectedProjectDirectoryName}`
+const expectedProjectConfigFileFullPath = `${expectedProjectFullPath}/4auth.json`
+const projectConfigJson = {
+  "project_id": "c69bc0e6-a429-11ea-bb37-0242ac130003",
+  "name": newProjectName,
+  "created_at": "2020-06-01T16:43:30+00:00",
+  "updated_at": "2020-06-01T16:43:30+00:00",
+  "credentials": [
+    {
+      "client_id": "6779ef20e75817b79602",
+      "client_secret": "dzi1v4osLNr5vv0.2mnvcKM37.",
+      "created_at": "2020-06-01T16:43:30+00:00"
+    }
+  ]
+}
 
-beforeEach(() => {
-  existsSyncStub = sinon.default.stub(fs, 'existsSync').withArgs(sinon.default.match(new RegExp(/config.json/)))
-  sinon.default.stub(fs, 'readJson').resolves(expectedUserConfig)
-  sinon.default.stub(inquirer, 'prompt').resolves({'projectName': 'hello'})
-  projectsApiCreateStub = sinon.default.stub(projectsModule.Projects.prototype, 'create').resolves({data: {name: 'hello'}})
-})
-
-afterEach(() => {
-  sinon.default.restore()
-});
+const projectNameWithLinks = newProjectName + ' with links'
+const expectedProjectConfigFileFullPathWithLinks = `${process.cwd()}/${expectedProjectDirectoryName}_with_links/4auth.json`
+const projectConfigJsonWithLinks = {
+  "project_id": "c69bc0e6-a429-11ea-bb37-0242ac130003",
+  "name": projectNameWithLinks,
+  "created_at": "2020-06-01T16:43:30+00:00",
+  "updated_at": "2020-06-01T16:43:30+00:00",
+  "credentials": [
+    {
+      "client_id": "6779ef20e75817b79602",
+      "client_secret": "dzi1v4osLNr5vv0.2mnvcKM37.",
+      "created_at": "2020-06-01T16:43:30+00:00"
+    }
+  ],
+  "_links": {
+    "self": {
+      "href": "https://eu.api.4auth.io/console/v1/projects/c69bc0e6-a429-11ea-bb37-0242ac130003"
+    }
+  }
+}
 
 describe('Command: projects:create', () => {
+
+  beforeEach(() => {
+    existsSyncStub = sinon.default.stub(fs, 'existsSync')
+    existsSyncStub.withArgs(sinon.default.match(new RegExp(/config.json/))).returns(true)
+
+    sinon.default.stub(fs, 'readJson').resolves(expectedUserConfig)
+    sinon.default.stub(inquirer, 'prompt').resolves({'projectName': newProjectName})
+    
+    projectsApiCreateStub = sinon.default.stub(projectsModule.Projects.prototype, 'create')
+    projectsApiCreateStub.withArgs({name: newProjectName}).resolves({data: projectConfigJson})
+  })
+  
+  afterEach(() => {
+    sinon.default.restore()
+  });
+
   test
-  .do( () => {
-    existsSyncStub.returns(true)
+  .do( () => {  
+    existsSyncStub.withArgs(sinon.default.match(expectedProjectFullPath)).returns(false)
+    projectConfigFileCreationStub = sinon.default.stub(fs, 'outputJson')
+    projectConfigFileCreationStub.resolves()
   })
   .command(['projects:create'])
   .it('prompts for the name of a project', ctx => {
-    expect(projectsApiCreateStub).to.have.been.calledWith({name: 'hello'})
+    expect(projectsApiCreateStub).to.have.been.calledWith({name: newProjectName})
   })
-
 
   test
   .do( () => {
-    existsSyncStub.returns(true)
+    existsSyncStub.withArgs(sinon.default.match(expectedProjectFullPath)).returns(false)
+    sinon.default.stub(fs, 'mkdir').resolves()
+
+    const inlineArgProject = {
+      ...projectConfigJson
+    }
+    inlineArgProject.name = 'inline arg name'
+
+    projectsApiCreateStub.withArgs({name: 'inline arg name'}).resolves({data: projectConfigJson})
+    projectConfigFileCreationStub = sinon.default.stub(fs, 'outputJson')
+    projectConfigFileCreationStub.resolves()
   })
   .command(['projects:create', 'inline arg name'])
-  .it('uses the inline_arg argument for the project', ctx => {
+  .it('uses the inline argument for the name project', ctx => {
     expect(projectsApiCreateStub).to.have.been.calledWith({name: 'inline arg name'})
   })
 
   test
   .do( () => {
-
+    existsSyncStub.withArgs(sinon.default.match(expectedProjectFullPath)).returns(true)
   })
   .command(['projects:create', 'My First Project'])
-  .it('creates a new directory base on the name of the project', ctx => {
+  .exit(1)
+  .it('errors if a directory matching the target project directory name already exists')
 
+  test
+  .do( () => {
+    existsSyncStub.withArgs(sinon.default.match(expectedProjectFullPath)).returns(false)
+    projectConfigFileCreationStub = sinon.default.stub(fs, 'outputJson')
+    projectConfigFileCreationStub.throws()
+  })
+  .command(['projects:create', 'My First Project'])
+  .exit(1)
+  .it('errors if an exception occurs when creating the project directory')
+
+  test
+  .do( () => {
+    existsSyncStub.withArgs(sinon.default.match(expectedProjectFullPath)).returns(false)
+
+    projectConfigFileCreationStub = sinon.default.stub(fs, 'outputJson')
+    projectConfigFileCreationStub.resolves()
+  })
+  .command(['projects:create', 'My First Project'])
+  .it('creates a 4auth.json project configuration file with the Project resource contents', ctx => {
+    expect(projectConfigFileCreationStub).to.have.been.calledWith(
+      expectedProjectConfigFileFullPath,
+      sinon.default.match(projectConfigJson)
+    )
   })
 
   test
   .do( () => {
+    existsSyncStub.withArgs(expectedProjectConfigFileFullPathWithLinks).returns(false)
 
+    projectsApiCreateStub.withArgs({name: projectConfigJsonWithLinks.name}).resolves({data: projectConfigJsonWithLinks})
+    projectConfigFileCreationStub = sinon.default.stub(fs, 'outputJson')
+    projectConfigFileCreationStub.resolves()
   })
-  .command(['projects:create', 'My First Project'])
-  .it('creates a new directory with a snake-case name based on the name of the project', ctx => {
+  .command(['projects:create', projectConfigJsonWithLinks.name])
+  .it('creates a 4auth.json project configuration file stripping the _links contents', ctx => {
 
-  })
+    const expectedSavedConfig = {
+      ...projectConfigJsonWithLinks
+    }
+    delete expectedSavedConfig._links
 
-  test
-  .do( () => {
-
-  })
-  .command(['projects:create', 'My First Project'])
-  .it('creates a 4auth.json project configuration file', ctx => {
-
-  })
-
-  test
-  .do( () => {
-
-  })
-  .command(['projects:create', 'My First Project'])
-  .it('saves project configuration into a 4auth.json project configuration file', ctx => {
-    
+    expect(projectConfigFileCreationStub).to.have.been.calledWith(
+      expectedProjectConfigFileFullPathWithLinks,
+      sinon.default.match(expectedSavedConfig)
+    )
   })
 
 })
