c3e0d5489d66d0112705ea69c195c70593ce4512
Phil Leggetter <phil@leggetter.co.uk>
2020-07-21 15:53:19 +0100

    Move abstract command so it's not listed as a user command

--- src/commands/projects/create.ts
+++ src/commands/projects/create.ts
@@ -1,5 +1,5 @@
 import {flags} from '@oclif/command'
-import CommandWithConfig from '../commandWithConfig'
+import CommandWithConfig from '../../helpers/commandWithConfig'
 import * as inquirer from 'inquirer'
 import {Projects} from '../../api/projects'
 import {APIConfiguration} from '../../api/APIConfiguration'
--- src/helpers/commandWithConfig.ts
+++ src/helpers/commandWithConfig.ts
@@ -2,7 +2,7 @@ import {Command} from '@oclif/command'
 import * as fs from 'fs-extra'
 import * as path from 'path'
 
-export default class extends Command {
+export default abstract class CommandWithConfig extends Command {
   userConfig: any
   async run() {
     this.userConfig = await fs.readJSON(path.join(this.config.configDir, 'config.json'))
