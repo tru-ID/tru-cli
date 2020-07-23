2e5818e0adfe2a7f895b91bd8197fcffd5e3bc62
Phil Leggetter <phil@leggetter.co.uk>
2020-07-23 15:46:31 +0100

    refactor: remove axios usage from ProjectsAPIClient

--- test/api/ProjectsAPIClient.test.ts
+++ test/api/ProjectsAPIClient.test.ts
@@ -2,10 +2,9 @@ import * as sinon from 'ts-sinon'
 import * as chai from 'chai';
 import * as sinonChai from 'sinon-chai'
 
-import axios, { AxiosInstance } from 'axios'
-
 import {ProjectsAPIClient} from '../../src/api/ProjectsAPIClient'
 import {APIConfiguration} from '../../src/api/APIConfiguration';
+import * as httpClientModule from '../../src/api/HttpClient'
 
 const expect = chai.expect;
 chai.use(sinonChai);
@@ -14,7 +13,8 @@ describe('API: projects', () => {
     const projectName:string = 'a project'
     const accessToken:string = 'i am an access token'
 
-    let axiosPostStub:any = null
+    let httpClientStub:any = null
+    let httpClientPostStub:any = null
 
     function createDefaultProjectsAPI():ProjectsAPIClient {
         return new ProjectsAPIClient(
@@ -27,48 +27,29 @@ describe('API: projects', () => {
     }
 
     beforeEach(() => {
-        sinon.default.stub(axios, 'create').returns(axios)
-        axiosPostStub = sinon.default.stub(axios, 'post')
-    
-        axiosPostStub.withArgs('/oauth2/v1/token', sinon.default.match.any, sinon.default.match.any).resolves({data: {access_token: accessToken}})
-        axiosPostStub.withArgs('/console/v0.1/projects', sinon.default.match.any, sinon.default.match.any).resolves({name: projectName})
+        httpClientPostStub = sinon.default.stub(httpClientModule.HttpClient.prototype, 'post')
+        httpClientPostStub.withArgs('/console/v0.1/projects', sinon.default.match.any, sinon.default.match.any).resolves({name: projectName})
     })
 
     afterEach(() => {
         sinon.default.restore()
     })
 
-    it('should make a request to create a Project with an access token', async () => {
-
-        const projectsAPI:ProjectsAPIClient = createDefaultProjectsAPI()
-
-        const result = await projectsAPI.create({name: projectName})
-
-        expect(axiosPostStub).to.have.been.calledWith(
-            sinon.default.match.any,
-            sinon.default.match.any,
-            sinon.default.match((arg) => {
-                return arg.headers['Authorization'] === `Bearer ${accessToken}`
-            })
-        )
-
-    })
-
-    it('should create a project with the expected name', async () => {
+    it('should make a request to create a project with the expected name', async () => {
         const projectsAPI:ProjectsAPIClient = createDefaultProjectsAPI()
 
         const projectName:string = 'a unique project name'
         await projectsAPI.create({name: projectName})
 
-        expect(axiosPostStub).to.have.been.calledWith(sinon.default.match.any, {name: projectName}, sinon.default.match.any)
+        expect(httpClientPostStub).to.have.been.calledWith(sinon.default.match.any, {name: projectName}, sinon.default.match.any)
     })
 
-    it('should create a project with the expected API endpoint path', async () => {
+    it('should make a request to create a project with the expected API endpoint path', async () => {
         const projectsAPI:ProjectsAPIClient = createDefaultProjectsAPI()
 
         const projectName:string = 'a unique project name'
         await projectsAPI.create({name: projectName})
 
-        expect(axiosPostStub).to.have.been.calledWith('/console/v0.1/projects', sinon.default.match.any, sinon.default.match.any)
+        expect(httpClientPostStub).to.have.been.calledWith('/console/v0.1/projects', sinon.default.match.any, sinon.default.match.any)
     })
 })
