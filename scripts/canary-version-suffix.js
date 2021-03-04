ef9c0ced14256025ef6f2ded7c8671fb62dca67e
Phil Leggetter <phil@leggetter.co.uk>
2021-03-04 11:09:38 +0000

    chore: .pkg build on `canary` branch

--- scripts/canary-version-suffix.js
+++ scripts/canary-version-suffix.js
@@ -2,31 +2,31 @@ const path = require('path')
 const fs = require('fs-extra')
 const os = require('os')
 
-const devBuildBranches = ['canary', 'ci-publish-test']
+const canaryBranches = ['canary', 'ci-publish-test']
 
 const buildBranch = process.env.CI_COMMIT_BRANCH
 const buildCommitSHA = process.env.CI_COMMIT_SHORT_SHA
 const packageFilePath = path.join(__dirname, '..', 'package.json')
 const packageJson = require(packageFilePath)
 
-if( isDevBuildBranch(buildBranch) &&
-    isNotDevVersion(packageJson.version) ) {
+function isCanaryBranch(branch) {
+    return branch && canaryBranches.indexOf(branch) !== -1
+}
+
+function isNotCanaryVersion(version) {
+    return version.indexOf('-canary') === -1
+}
+
+if( isCanaryBranch(buildBranch) &&
+isNotCanaryVersion(packageJson.version) ) {
     
-    console.log('Is Dev Build Branch. Appending "-dev" to package.json version for build...')
+    console.log('Is Dev Build Branch. Appending "-canary" to package.json version for build...')
     
-    const devVersion = `${packageJson.version}-dev.${buildCommitSHA}`
+    const devVersion = `${packageJson.version}-canary.${buildCommitSHA}`
     console.log('writing package.json with version', devVersion)
 
     packageJson.version = devVersion
     fs.writeJSONSync(packageFilePath, packageJson, {spaces: 2,EOL: os.EOL})
 
     console.log('writing package.json complete')
-}
-
-function isDevBuildBranch(branch) {
-    return branch && devBuildBranches.indexOf(buildBranch) !== -1
-}
-
-function isNotDevVersion(version) {
-    return version.indexOf('-dev') === -1
 }
