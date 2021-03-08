6f045365a652d61fc720ea5bdbb7b4e58a8dd552
Phil Leggetter <phil@leggetter.co.uk>
2021-03-08 17:49:24 +0000

    fix: init hook should not block setup:credentials from running

--- test/commands/check-config.test.ts
+++ test/commands/check-config.test.ts
@@ -1,10 +1,16 @@
-import {expect, test} from '@oclif/test'
+import {test} from '@oclif/test'
+import * as chai from 'chai'
+import * as sinonChai from 'sinon-chai'
 import * as sinon from 'ts-sinon'
+
+const expect = chai.expect
+chai.use(sinonChai)
 
 import * as fs from 'fs-extra'
 
 describe('hooks', () => {
   let existsSyncStub: any = null
+  let outputJsonStub: any = null
   const expectedUserConfig = {
     defaultWorkspaceClientId: 'my client id',
     defaultWorkspaceClientSecret: 'my client secret',
@@ -32,8 +38,30 @@ describe('hooks', () => {
   .stdout()
   .hook('init')
   .exit(1)
-  .it('should instruct the user to go to tru.id/console', ctx => {
+  .it('should instruct the user to go to developer.tru.id/console', ctx => {
     expect(ctx.stdout).to.contain('Welcome to the tru.ID CLI!')
-    expect(ctx.stdout).to.contain('tru.id/console')
+    expect(ctx.stdout).to.contain('developer.tru.id/console')
+  })
+
+  test
+  .do(() => {
+    existsSyncStub = sinon.default.stub(fs, 'existsSync').withArgs(sinon.default.match(new RegExp(/config.json/))).returns(false)
+  })
+  .stdout()
+  .command('phonechecks:create')
+  .exit(1)
+  .it('should instruct the user to go to developer.tru.id/console when no config file is present and a command is run', ctx => {
+    expect(ctx.stdout).to.contain('Welcome to the tru.ID CLI!')
+    expect(ctx.stdout).to.contain('developer.tru.id/console')
+  })
+
+  test
+  .do(() => {
+    existsSyncStub = sinon.default.stub(fs, 'existsSync').withArgs(sinon.default.match(new RegExp(/config.json/))).returns(false)
+    outputJsonStub = sinon.default.stub(fs, 'outputJson').resolves()
+  })
+  .command(['setup:credentials', expectedUserConfig.defaultWorkspaceClientId, expectedUserConfig.defaultWorkspaceClientSecret, expectedUserConfig.defaultWorkspaceDataResidency])
+  .it('should NOT instruct the user to setup the CLI if the user is running the `setup:credentials` command', () => {
+    expect(outputJsonStub).to.have.been.calledWith(sinon.default.match.any, expectedUserConfig, sinon.default.match.any)
   })
 })
